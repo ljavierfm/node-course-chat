0ca8d936f9d1e56bbb0306c6ebf8eb048bf9ba5e
Javier Fernandez <ljavierfm@gmail.com>
2017-12-02 19:07:34 +0100

    Connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@
 
 //imports
 const path=require('path');
+const http=require('http');
 const express=require('express');
+const socketIO=require('socket.io');
 
 //public path
 const publicPath=path.join(__dirname,'../public');
@@ -11,12 +13,20 @@ const port=process.env.PORT||3000;
 
 //Creates an Express application
 let app=express();
+let server=http.createServer(app);
+let io=socketIO(server);
 
 //middleware. serves static files
 app.use(express.static(publicPath));
 
+//registra un listener
+//socket representa uan conexion deu n cliente
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+});
+
 //Start de server
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server starting at ${port}`);
 });
 
