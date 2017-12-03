6aa9846cb59a25ae13a7e3fc292bbcb1e0511ed2
Javier Fernandez <ljavierfm@gmail.com>
2017-12-03 11:15:46 +0100

    Greet new user, and alter

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 /*jshint esversion: 6 */
 
+/*
+io.emit - sends to all connected users
+
+socket.emit - sends to current connected user
+
+socket.broadcast.emit - sends to all connected users but the initiator
+*/
+
 //imports
 const path=require('path');
 const http=require('http');
@@ -24,15 +32,37 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New user connected');
 
+    //Admin text Welcome to the chat app
+    socket.emit('newMessage',{
+        from:'Admin',
+        text:'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    //Admin sends new user Connect message to all users
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'A new user joined',
+        createdAt:new Date().getTime()
+    });
+
     //Se dispara cuando llega un evento desde un cliente tipo createMesage
     socket.on('createMessage',(message)=>{
         console.log('createMessage',message);
+
         //transmite un evento a cada una de las conexiones establecidas
         io.emit('newMessage',{
             from:message.from,
             text:message.text,
             createdAt:new Date().getTime()
         });
+
+        //Se transmite a todos menos al emisor
+        /* socket.broadcast.emit('newMessage',{
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        }); */
     });
 
     socket.on('disconnect',()=>{
